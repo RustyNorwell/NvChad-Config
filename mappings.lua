9d75888ab84b9b86ae6d4594d7b745ad9fce7cc8
Martin Jiroušek <jirouma5@fel.cvut.cz>
2024-06-08 13:55:10 +0000

    Add basic mappings

---
+++ mappings.lua
@@ -0,0 +1,31 @@
+-- ~/.config/nvim/lua/custom/mappings.lua
+
+local M = {}
+
+M.general = {
+  -- normal mode
+  n = {
+    ["<C-h>"] = {"<cmd> TmuxNavigateLeft<CR>", "window left"},
+    ["<C-l>"] = {"<cmd> TmuxNavigateRight<CR>", "window right"},
+    ["<C-j>"] = {"<cmd> TmuxNavigateDown<CR>", "window down"},
+    ["<C-k>"] = {"<cmd> TmuxNavigateUp<CR>", "window up"},
+    ["ss"] = {":w<CR>", "save file"},
+  },
+
+  -- insert mode
+  i = {
+    ["jk"] = {"<Esc>", "escape insert mode"},
+  }
+}
+
+-- local map = vim.api.nvim_set_keymap
+local options = { noremap = true, silent = true }
+
+-- Set the mappings
+for mode, mappings in pairs(M.general) do
+  for key, map in pairs(mappings) do
+    vim.api.nvim_set_keymap(mode, key, map[1], options)
+  end
+end
+
+return M
